cbd2d2f23db750aec9a511b9f1b4cb5c69d39517
huangluzhi <314726262@qq.com>
2022-04-06 20:08:57 +0800

    web demo debug

--- demo/demo/tagging_data_view.py
+++ demo/demo/tagging_data_view.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render
 from django.views.decorators import csrf
 import thulac
- 
+
 import sys
 sys.path.append("..")
 from toolkit.pre_load import neo_con
@@ -15,32 +15,32 @@ def showtagging_data(request):
 		# 连接数据库
 		db = neo_con
 		title = request.GET['title']
-		answer = db.matchHudongItembyTitle(title)
+		answer = db.matchHudongItembyTitle(title)[0]['n']
 		if answer == None:
 			ctx['title'] = '<h1> 该url不存在，别乱搞！ </h1>'
 			return render(request, "tagging_data.html", ctx)
 		ctx['detail'] = answer['detail']
 		ctx['title'] = answer['title']
 		image = answer['image']
-		
+
 		ctx['image'] = '<img class="rounded card-img-top img-fluid" src="' + str(image) + '" alt="该条目无图片" style="width:30%" >'
-		
+
 		ctx['baseInfoKeyList'] = []
 		List = answer['baseInfoKeyList'].split('##')
 		for p in List:
 			ctx['baseInfoKeyList'].append(p)
-			
+
 		ctx['baseInfoValueList'] = []
 		List = answer['baseInfoValueList'].split('##')
 		for p in List:
 			ctx['baseInfoValueList'].append(p)
-			
+
 		text = ""
 		List = answer['openTypeList'].split('##')
 		for p in List:
 			text += '<span class="badge badge-success">' + str(p) + '</span> '
 		ctx['openTypeList'] = text
-		
+
 		text = ""
 		keyList = answer['baseInfoKeyList'].split('##')
 		valueList = answer['baseInfoValueList'].split('##')
@@ -53,7 +53,7 @@ def showtagging_data(request):
 			text += '<td class="font-weight-bold">' + keyList[i] + '</td>'
 			text += '<td>' + value + '</td>'
 			i += 1
-			
+
 			if i < len(valueList):
 				value = valueList[i]
 			if i < len(keyList) :
@@ -65,9 +65,9 @@ def showtagging_data(request):
 			i += 1
 			text += "</tr>"
 		ctx['baseInfoTable'] = text
-		
+
 		## 动态生成check控件----------------------------------
-		
+
 		text = ""
 		tag_name_list = []
 		tag_name_list.append('Invalid（不合法，不是具体的实体，或一些脏数据，或与农业毫无关系）')
@@ -87,7 +87,7 @@ def showtagging_data(request):
 		tag_name_list.append('Agricultural implements（农机具，一般指机械或物理设施）')
 		tag_name_list.append('Technology(农业相关术语，技术和措施)')
 		tag_name_list.append('other（除上面类别之外的其它名词实体，可以与农业无关但必须是实体）')
-		
+
 		count = 0
 		for i in range(len(tag_name_list)):
 			text += '<div class="radio"> <label class="form-check-label">'
@@ -95,12 +95,12 @@ def showtagging_data(request):
 			text +=  str(count) + '. ' + tag_name_list[i]
 			text += '</label>  </div>'
 			count += 1
-			
+
 		# 放置一个隐藏的输入框，传递title的值到缓冲页面
 		text += '<input name="title" value="' + str(answer['title']) + '"  style="display:none;" ></input>'
 		ctx['taggingCheck'] = text
-		
-		
+
+
 		# 统计当前标注情况
 		file_object = open('label_data/labels.txt','r')
 		s = []
@@ -110,7 +110,7 @@ def showtagging_data(request):
 		for f in file_object:
 			pair = f.split()
 			s[int(pair[1].strip())].add(pair[0].strip())
-		for i in range(17):	
+		for i in range(17):
 			sum += len(s[i])
 		file_object.close()
 		text = "" ##用于记录已标注样本个数
@@ -119,6 +119,5 @@ def showtagging_data(request):
 		text += '<p>总计: ' + str(sum) + '个</p>'
 		ctx['already'] = text
 
-				
+
 	return render(request, "tagging_data.html", ctx)
-	
